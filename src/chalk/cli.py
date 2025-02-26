655bd0cc06335f4a3dfa5b69cc8624f83d9cb129
Kang Chen <kangchen@nypl.org>
2025-02-26 07:33:20 -0500

    added command line args interface

--- src/chalk/cli.py
+++ src/chalk/cli.py
@@ -1,4 +1,3 @@
-from typing import Required
 import click
 
 import pathlib
@@ -24,60 +23,252 @@ def validate_duration():
     raise
 
 
-@click.group()
-def zoom():
-    """Schedule Zoom meetings"""
-    pass
-
-
-@zoom.command()
-@click.option("-c", "--class", help="Topic of the meeting (classname).")
-@click.option("-D", "--date", required=True, help="Date of meeting.")
-@click.option("-t", "--time", help="Start time of the meeting.")
-@click.option(
-    "-d",
-    "--duration",
-    type=int,
-    help="Duration of the meeting in n times 15 minutes",
-)
-@click.option(
-    "--from-schedule", is_flag=True, help="Creates meeting from config/schedule"
-)
-def schedule(date, time, length):
-    """Schedule Zoom classes."""
-    click.echo(f"Scheduling classes on {date} with time {time} for {length} minutes.")
-
-
-@click.group()
-def mail():
-    """Manage gmail tasks."""
-    pass
-
-
-@mail.command()
-@click.option("-d", "--draft", is_flag=True, help="Draft an email based on template.")
-@click.option("-S", "--send", is_flag=True, help="Send an email based on template.")
-@click.option("-s", "--schedule", is_flag=True, help="Schedule an email.")
-@click.option("--template", required=True, help="Email template to use.")
-@click.option("--date", help="Schedule email on this date.")
-@click.option("--time", help="Schedule email at this time.")
-def email(draft, send, schedule, template, date, time):
-    """Handle email operations."""
-    if draft:
-        click.echo(f"Drafting email using template {template}.")
-    elif send:
-        click.echo(f"Sending email using template {template}.")
-    elif schedule:
-        click.echo(f"Scheduling email using template {template} on {date} at {time}.")
-    else:
-        click.echo("No action specified.")
+# -------------------------------
+# CLI Entry Point
+# -------------------------------
 
 
 @click.group()
 def cli():
-    """Chalk CLI"""
+    """Chalk - TechConnect CLI automation tool."""
     pass
 
 
-cli.add_command(zoom)
-cli.add_command(mail)
+# -------------------------------
+# Zoom Commands
+# -------------------------------
+
+
+@cli.group()
+def zoom():
+    """Manage Zoom meetings."""
+    pass
+
+
+@zoom.command(context_settings={"ignore_unknown_options": True})
+@click.argument("topic", required=True, type=str)
+@click.option("-d", "--date", required=True, help="Schedule meeting date.")
+@click.option("-t", "--time", required=True, help="Specify meeting start time.")
+@click.option(
+    "-D",
+    "--duration",
+    required=True,
+    type=int,
+    help="Duration of the meeting in n times 15 minutes.",
+)
+@click.option(
+    "--from-schedule",
+    is_flag=True,
+    help="Create meetings from config/schedule, --month or --date argument required.",
+)
+@click.option(
+    "-m", "--month", help="Create meetings of this month, used with --from-schedule."
+)
+@click.option(
+    "-h",
+    "--headless-mode",
+    is_flag=True,
+    default=False,
+    help="Use Playwright in headless mode.",
+)
+def create(topic, date, time, duration, from_schedule, month, headless_mode):
+    """(Not implemented) Create a Zoom meeting with a topic."""
+    if from_schedule:
+        if not month and not date:
+            raise click.UsageError(
+                "--from-schedule flag requires --month or --date argument."
+            )
+        if month:
+            click.echo(f"Creating Zoom meetings for {month} from schedule.")
+        if date:
+            click.echo(f"Creating Zoom meetings on {date} from schedule.")
+    else:
+        click.echo(
+            f"Creating Zoom meeting: '{topic}' on {date} at {time} for {duration} minutes. (schedule?: {from_schedule}, headless-mode?: {headless_mode})"
+        )
+
+
+# -------------------------------
+# Gmail Commands TODO:
+# -------------------------------
+
+
+@cli.group()
+def mail():
+    """Manage Gmail tasks."""
+    pass
+
+
+@mail.command(context_settings={"ignore_unknown_options": True})
+@click.option("-T", "--template", required=True, help="Email template to use.")
+def draft(template):
+    """(Not implemented) Draft an email using a template."""
+    click.echo(f"Drafting email using template {template}.")
+
+
+@mail.command(context_settings={"ignore_unknown_options": True})
+@click.option("-T", "--template", required=True, help="Email template to use.")
+def send(template):
+    """(Not implemented) Send an email using a template."""
+    click.echo(f"Sending email using template {template}.")
+
+
+@mail.command(context_settings={"ignore_unknown_options": True})
+@click.option("-T", "--template", required=True, help="Email template to use.")
+@click.option("-d", "--date", required=True, help="Schedule email on this date.")
+@click.option("-t", "--time", required=True, help="Schedule email at this time.")
+def schedule(template, date, time):
+    """(Not implemented) Schedule an email to be sent later."""
+    click.echo(f"Scheduling email using template {template} on {date} at {time}.")
+
+
+# -------------------------------
+# Google Calendar Commands TODO:
+# -------------------------------
+
+
+@cli.group()
+def calendar():
+    """Manage Google Calendar events."""
+    pass
+
+
+@calendar.command(context_settings={"ignore_unknown_options": True})
+@click.argument("event-topic", required=True, type=str)
+@click.option("-d", "--date", required=True, help="Create calendar event on this date.")
+@click.option("-t", "--time", required=True, help="Create calendar event at this time.")
+def create(event_topic, date, time):
+    """(Not implemented) Create calendar event."""
+    click.echo(f"Creating calendar event '{event_topic}' on {date} at {time}.")
+
+
+@calendar.command(context_settings={"ignore_unknown_options": True})
+@click.option("-d", "--date", help="Create calendar event on this date.")
+@click.option("-t", "--time", help="Create calendar event at this time.")
+@click.option("-a", "--all", is_flag=True, help="Create all events from schedule.")
+def create_from_schedule(date, time, all):
+    """(Not implemented) Create calendar events from schedule."""
+    click.echo(f"Creating events on {date} at {time}. (all?: {all})")
+
+
+# -------------------------------
+# Drupal Event Commands TODO:
+# -------------------------------
+
+
+@cli.group()
+def classes():
+    """Manage Drupal events."""
+    pass
+
+
+@classes.command(context_settings={"ignore_unknown_options": True})
+@click.option(
+    "-d", "--date", required=True, help="Get registration emails of class on this date."
+)
+@click.option(
+    "-t", "--time", required=True, help="Get registration emails of class at this time."
+)
+def get_registration(date, time):
+    """(Not implemented) Get registration emails."""
+    click.echo(f"Getting registration emails of class on {date} at {time}.")
+
+
+@classes.command(context_settings={"ignore_unknown_options": True})
+@click.option("-d", "--date", help="Create class on this date.")
+@click.option("-t", "--time", help="Create class at this time.")
+@click.option(
+    "--from-schedule",
+    is_flag=True,
+    help="Create events from schedule, --month argument required.",
+)
+@click.option(
+    "-m",
+    "--month",
+    help="Create all drupal events of this month, used with --from-schedule.",
+)
+def create(date, time, from_schedule, month):
+    """(Not implemented) Create Drupal event."""
+    if from_schedule:
+        if not month:
+            raise click.UsageError("--from_schedule flag requires --month argument.")
+        click.echo(f"Creating Drupal event of {month}.")
+    else:
+        click.echo(f"Creating Drupal events on {date} at {time}.")
+
+
+# -------------------------------
+# Pipeline Commands TODO:
+# -------------------------------
+
+
+@cli.group()
+def pipeline():
+    """Automation pipelines. Requires a schedule."""
+    pass
+
+
+@pipeline.command(context_settings={"ignore_unknown_options": True})
+@click.option("-d", "--date", required=True, help="Schedule meeting date.")
+@click.option("-t", "--time", required=True, help="Specify meeting start time.")
+@click.option("-T", "--template", required=True, help="Email template to use.")
+def schedule_and_email_one(date, time, template):
+    """(Not implemented) Scheule Zoom meeting and draft email with template and registration list (if available) for one class."""
+    click.echo(
+        f"Schedule meeting at {date} {time} and creating email draft with {template}."
+    )
+
+
+@pipeline.command(context_settings={"ignore_unknown_options": True})
+@click.option("-d", "--dates", required=True, type=list, help="Schedule meeting date.")
+@click.option("-T", "--template", required=True, help="Email template to use.")
+def schedule_and_email_many(dates, template):
+    """(Not implemented) Scheule Zoom meetings and draft emails with template and registration list (if available) for multiple classes."""
+    click.echo(
+        f"Schedule meetings for {dates} and creating email drafts with {template}."
+    )
+
+
+@pipeline.command(context_settings={"ignore_unknown_options": True})
+@click.option(
+    "-D", "--dates", type=list, help="Create drupal and calendar events for dates."
+)
+@click.option("-d", "--date", help="Create drupal and calendar event for this date.")
+@click.option("-t", "--time", help="Create drupal and calendar event for this time.")
+def drupal_and_calendar(dates, date, time):
+    """(Not implemented) Create Drupal and Calender event(s)."""
+    if dates:
+        click.echo(f"Creating Drupal and Calendar events for {dates}.")
+    else:
+        if date and time:
+            click.echo(f"Creating Drupal and Calendar event at {date} {time}.")
+        else:
+            raise click.UsageError(
+                "--date and --time arguments are both required when creating singular event."
+            )
+
+
+@pipeline.command(context_settings={"ignore_unknown_options": True})
+@click.option(
+    "-D",
+    "--dates",
+    type=list,
+    help="Create drupal, calendar, and zoom events for dates.",
+)
+@click.option(
+    "-d", "--date", help="Create drupal, calendar, and zoom event for this date."
+)
+@click.option(
+    "-t", "--time", help="Create drupal, calendar, and zoom event for this time."
+)
+def drupal_and_calendar_and_zoom(dates, date, time):
+    """(Not implemented) Create Drupal, Calender, and Zoom event(s)."""
+    if dates:
+        click.echo(f"Creating Drupal, Calender, and Zoom events for {dates}.")
+    else:
+        if date and time:
+            click.echo(f"Creating Drupal, Calender, and Zoom event at {date} {time}.")
+        else:
+            raise click.UsageError(
+                "--date and --time arguments are both required when creating singular event."
+            )

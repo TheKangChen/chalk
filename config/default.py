98a59556f9736783df04f2a1f1c7dc73ef7046a4
Kang Chen <kangchen@nypl.org>
2025-02-26 19:51:00 -0500

    config & .env file example

---
+++ config/default.py
@@ -0,0 +1,18 @@
+# google api scopes
+GOOGLE_API_SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets.readonly",
+    "https://mail.google.com/",
+    "https://www.googleapis.com/auth/calendar.events.owned",
+]
+
+# google api tokens
+CRED_DIR = "./credentials"
+CRED_PATH = f"{CRED_DIR}/credentials.json"
+TOKEN_PATH = f"{CRED_DIR}/token.json"
+
+# config default directory
+CONFIG_DIR = "./config"
+DEFAULT_CONF = f"{CONFIG_DIR}/default.py"
+
+# logs directory
+LOG_DIR = "./run_logs"

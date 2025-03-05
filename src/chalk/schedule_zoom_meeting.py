a575cadfa7c442372456a20c6dde317549a56ba1
Kang Chen <kangchen@nypl.org>
2025-03-05 02:37:42 -0500

    schedule class

--- src/chalk/schedule_zoom_meeting.py
+++ src/chalk/schedule_zoom_meeting.py
@@ -7,32 +7,35 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".
 
 from datetime import datetime
 
+from chalk.chalk_utils.env_vars import get_env_vars
 from chalk.zoom.scheduler import (
     ZoomLoginCreds,
     ZoomMeetingConfig,
     schedule_zoom_meetings,
 )
-from config.config import nypl_email, zoom_password
+# from config.default import nypl_email, zoom_password
 
 if __name__ == "__main__":
+    creds = get_env_vars()
     headless_mode = True
-    login_creds = ZoomLoginCreds(email=nypl_email, password=zoom_password)
+    login_creds = ZoomLoginCreds(email=creds["NYPL_EMAIL"], password=creds["ZOOM_PASSWORD"])
     meeting_configs = [
+        # BUG: When schedule on same day, it sometimes schedule the day of the next month
         ZoomMeetingConfig(
-            topic="Excel Genius Class 4: Tables and Data Validation",
-            date=datetime(2025, 2, 26, 11, 0),
+            topic="Excel Genius Class 5: Pivot Tables",
+            date=datetime(2025, 3, 5, 11, 0),
             duration_hours=1,
             duration_minutes=30,
         ),
         ZoomMeetingConfig(
-            topic="JavaScript Basics Part 1",
-            date=datetime(2025, 2, 26, 15, 30),
+            topic="Advanced Powerpoint",
+            date=datetime(2025, 3, 5, 15, 30),
             duration_hours=1,
             duration_minutes=30,
         ),
         ZoomMeetingConfig(
-            topic="Consuming Web APIs with Python",
-            date=datetime(2025, 2, 27, 11, 0),
+            topic="Web Scraping with Python Part 1",
+            date=datetime(2025, 3, 6, 11, 0),
             duration_hours=1,
             duration_minutes=30,
         ),
@@ -42,8 +45,10 @@ if __name__ == "__main__":
         meeting_infos = schedule_zoom_meetings(
             meeting_configs, login_creds, headless_mode
         )
-        for m in meeting_infos:
+        for c, m in zip(meeting_configs, meeting_infos):
+            print(c.topic)
             print(f"Meeting Link: {m.meeting_link}")
+            print()
             print(f"Meeting ID: {m.meeting_id}")
             print(f"Passcode: {m.passcode}")
 

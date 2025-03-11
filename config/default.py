293a398cc96791e968efe7aefc4dc73d66ad4f2f
Kang Chen <kangchen@nypl.org>
2025-03-10 20:48:57 -0400

    update config

--- config/default.py
+++ config/default.py
@@ -10,9 +10,16 @@ CRED_DIR = "./credentials"
 CRED_PATH = f"{CRED_DIR}/credentials.json"
 TOKEN_PATH = f"{CRED_DIR}/token.json"
 
+# logs directory
+LOG_DIR = "./run_logs"
+
+# database
+DB_PATH = "./db/chalk_schedule.sqlite"
+
 # config default directory
 CONFIG_DIR = "./config"
 DEFAULT_CONF = f"{CONFIG_DIR}/default.py"
 
-# logs directory
-LOG_DIR = "./run_logs"
+# schedule & active classes csv directory
+SCHEDULE_DIR = f"{CONFIG_DIR}/schedule"
+ACTIVE_CLASSES = f"{CONFIG_DIR}/schedule/active_classes.csv"
